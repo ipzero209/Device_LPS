014eb50a255b5ad5c707a74cf5ee28d4c48fff52
Craig Stancill <npl127.0.0.1@gmail.com>
2016-06-14 12:21:06 -0700

    Functional update
    Modified so that the script can be used against either a firewall or
    Panorama.

--- device_lps.py
+++ device_lps.py
@@ -7,6 +7,9 @@ import fileinput
 import sys
 
 print "Average LPS will be printed to the screen and can also be found at the bottom of the samples file with today's date."
+# import pudb; pudb.set_trace()
+# device_type = sys.argv[1]
+
 
 infile = open('device_lps.txt', 'r')
 outfile = open('samples.log', 'w+')
@@ -14,15 +17,23 @@ samples = []
 
 
 
-
 for line in infile:
-	if "incoming" in line:
-		curr_sample = line[30:50]
+	if "Log incoming rate" in line:
+		curr_sample = line[30:]
 		curr_sample = ''.join(curr_sample.split())
 		curr_sample = curr_sample[:-4]
-		samples.append(int(curr_sample))
+		samples.append(float(curr_sample))
 		outfile.write(curr_sample)
 		outfile.write('\n')
+	elif "Incoming log rate" in line:
+		curr_sample = line[20:]
+		curr_sample = ''.join(curr_sample.split())
+		samples.append(float(curr_sample))
+		outfile.write(curr_sample)
+		outfile.write('\n')
+
+
+
 
 total_of_samples = sum(samples)
 num_of_samples = len(samples)
